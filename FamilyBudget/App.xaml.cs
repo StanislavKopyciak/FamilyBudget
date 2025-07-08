14eeb8ec21270c72cbad27c14470a01a109b2234
Stanislav <StasKopicak@gmail.com>
2025-07-08 19:00:42 +0300

    Пустий проект

---
+++ FamilyBudget/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FamilyBudget
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ FamilyBudget/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FamilyBudget
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
